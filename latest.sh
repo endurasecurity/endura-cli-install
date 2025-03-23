4368d653501cc644846258c79f741a58d6795973
esheri3 <esheri3@users.noreply.github.com>
2025-03-23 21:27:34 +0000

    deploy: 21611955364da28bf931f4a04efcf17195221456

--- latest.sh
+++ latest.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env sh
+#
+# Intended to be installed via the following command:
+# curl -sSf https://endurasecurity.github.io/endura-cli-install/dist/latest.sh | sudo -E sh
+
+set -eu
+
+CHANNEL=endura-repo-latest
 ASC_URL="https://endurasecurity.github.io/${CHANNEL}/endura-cli-tools/endura.asc"
 DEB_URL="https://endurasecurity.github.io/${CHANNEL}/endura-cli-tools/deb"
 MISSING_CMDS=""
--- src/testing.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-#
-# Intended to be installed via the following command:
-# curl -sSf https://endurasecurity.github.io/endura-cli-install/dist/testing.sh | sudo -E sh
-
-set -eu
-
-CHANNEL=endura-repo-testing
